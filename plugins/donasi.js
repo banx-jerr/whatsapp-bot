b6e03678e09b2496ba1536a6f0559021cc281f51
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-01 14:59:40 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,14 +2,14 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(reyganz + 'APIKEY')).buffer(), `
 *O W N E R*
 
-*SAWERIA:* https://saweria.co/inirey
+*SAWERIA:* https://saweria.co/PangeranLord
 *TRAKKER:* https://trakteer.id/erzabot
 
 *P E N U L I S*
 
 *SAWERIA:* https://saweria.co/ariffb
 *TRAKKER:* https://trakteer.id/ariffb/tip
-`.trim(), '© sekha', 'Donasi', '.donasi', m)
+`.trim(), '© pangeran', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
